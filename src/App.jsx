3d842abe0bcc9e7781f4c899c08a41818127431a
ByteBloom <kojofrimpong631@gmail.com>
2026-05-10 04:14:52 -1100

    putting the routes together and setting the theme for dark and light modes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from "./context/theme.tsx";
+import Homepage from "./content/homepage";
+import SolutionsPage from "./content/solutionsPage";
+import SessionCreation from "./content/sessionCreation";
+import StudentCheckIn from "./content/studentCheckIn";
+import PricingPage from "./content/pricingPage";
+import ResourcesPage from "./content/resourcePage.tsx";
+
 function App() {
   return(
-      <NavigationProvider>
-      <Router>
-        <Routes>
-        </Routes>
-      </Router>
-      </NavigationProvider>
+      <ThemeProvider>
+          <Router>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/solutions" element={<SolutionsPage />} />
+                <Route path="/pricing" element={<PricingPage />} />
+                <Route path="/resources" element={<ResourcesPage />} />
+                <Route path="/session-creation" element={<SessionCreation />} />
+                <Route path="/student-check-in/:sessionId" element={<StudentCheckIn />} />
+            </Routes>
+          </Router>
+      </ThemeProvider>
   )
 }
 
